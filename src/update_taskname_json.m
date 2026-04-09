406ad97eb03e641eed3dc37169ee431fcd806c69
David López García <dlopez@ugr.es>
2026-04-09 15:34:36 +0200

    buf fix: taskName is also added to the phase sbref json files

--- src/update_taskname_json.m
+++ src/update_taskname_json.m
@@ -2,7 +2,7 @@ function update_taskname_json(cfg,dcm)
 %% FUNCTIONAL: TaskName problem in the sidecar JSON file:
 if strcmp(dcm.data_type, 'func') && isfield(dcm, 'task')
 
-    % Update TaskName field:
+    % Define TaskName field:
     fields.TaskName = dcm.task;
 
     % Get JSON file:
@@ -12,5 +12,21 @@ if strcmp(dcm.data_type, 'func') && isfield(dcm, 'task')
     update_json(json_file,fields)
 end
 
+%% SBREF: Phase part of the sbref files:
+if strcmp(dcm.data_type, 'func') && isfield(dcm,'task')
+
+    if isfield(cfg,'file_name_phase_sbref')
+
+        % Define TaskName field:
+        fields.TaskName = dcm.task;
+
+        % Get JSON file:
+        json_file = fullfile(cfg.out_folder, cfg.file_name_phase_sbref + ".json");
+
+        % Update json file:
+        update_json(json_file,fields)
+
+    end
+end
 end
 
